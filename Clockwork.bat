0cb8a34bebbe872fa4cbf2103b41848e14900240
rickyvr-byte <officialrickyvr@gmail.com>
2025-12-19 16:19:26 +1300

    Updated

--- Clockwork.bat
+++ Clockwork.bat
@@ -1,39 +1,6 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFCldTw+bAE+1EbsQ5+n//NaqrUMWUaw2e4C7
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFCldTw+bAE+1EbsQ5+n//NakrkIeX/UwaoSV36yLQA==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal enabledelayedexpansion
-title Clockwork - GTAG Plugin tool
+title Clockwork GTAG
 color 0A
 
 :: Configuration
@@ -92,7 +59,9 @@ if /i "%REPO_MODE%"=="unofficial" (
 :MAIN_MENU
 cls
 echo ===============================================
-echo    CLOCKWORK - GTAG PLUGIN TOOL
+echo    CLOCKWORK GTAG PLUGIN MANAGER
+echo ===============================================
+echo         By Ricky.zero
 echo ===============================================
 echo.
 echo 1. Install Plugins
